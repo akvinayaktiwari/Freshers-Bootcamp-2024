934c4978321e8b26ff0a91ce55204d67df074250
akvinayaktiwari <64476491+akvinayaktiwari@users.noreply.github.com>
2024-01-09 14:08:48 +0530

    filterFunction.cpp

--- Day2.cpp
+++ Day2.cpp
@@ -2,18 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool search(vector<int> arr,int tar){
-    int n=arr.size();
-    for(int i=0;i<n;i++){
-        if(arr[i]==tar) return 1;
+vector<int> filter(vector<int>& arr,function<bool<int>> condition){
+    vector<int> results;
+    for(auto element:arr){
+        if(condition(element)) results.push_back(element);
     }
-    return 0;
+    return results;
 }
 int main() {
     // Write C++ code here
-    vector<int> arr{1,2,12,23,45,23,12,5,80,76};
-    int n=15;
-    cout<<search(arr,n);
+    // vector<int> arr{1,2,12,23,45,23,12,5,80,76};
+    // int n=15;
+    // cout<<search(arr,n);
 
     return 0;
 }
