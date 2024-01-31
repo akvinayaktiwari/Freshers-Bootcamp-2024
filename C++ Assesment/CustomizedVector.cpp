b87e42280f50620527c6b75572615e569210805b
akvinayaktiwari <64476491+akvinayaktiwari@users.noreply.github.com>
2024-01-31 15:09:14 +0530

    Update CustomizedVector.cpp

--- C++ Assesment/CustomizedVector.cpp
+++ C++ Assesment/CustomizedVector.cpp
@@ -1,59 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class A{
+    int x;
+    public:
+    void setX(int xArg){
+        x=xArg;
+    }
+    int getX(){
+        return x;
+    }
+    ~A(){
+        cout<<"A is destructed"<<endl;
+    }
+};
 
-class DynamicIntegerArray {
+template <typename T>
+class DynamicArray {
     private:
-        int* arr; 
+        T* arr; 
         int capacity; 
         int size; 
     public:
-       
-        DynamicIntegerArray(int cap) {
-            arr = new int[cap]; 
+        DynamicArray(int cap) {
+            arr = new T[cap]; 
             capacity = cap; 
             size = 0;
         }
-
-        void add(int index, int value) {
+        void add(int index, T value) {
             if (index < 0 || index > size) { 
                 cout << "Invalid index" << endl;
                 return;
             }
             if (size == capacity) { 
-                int* newArr = new int[capacity * 2];
-                
+                T* newArr = new T[capacity * 2];
                 for (int i = 0; i < size; i++) {
                     newArr[i] = arr[i];
                 }
-                
                 delete[] arr;
-                
                 arr = newArr;
                 capacity = capacity * 2;
             }
-            
             for (int i = size; i > index; i--) {
                 arr[i] = arr[i - 1];
             }
-          
             arr[index] = value;
            
             size++;
         }
-
-        
-        int get(int index) const {
+        T get(int index) const {
             if (index < 0 || index >= size) { 
                 cout << "Invalid index" << endl;
-                return -1; 
+                return T(); 
             }
             return arr[index];
         }
-
-        
         void clear() {
-            
             delete[] arr;
             arr = nullptr;
             capacity = 0;
@@ -63,42 +65,42 @@ class DynamicIntegerArray {
             return size;
         }
         
-        bool operator<( const DynamicIntegerArray &arr) const 
+        bool operator<( const DynamicArray &arr) const 
     	{ 
     		return capacity < arr.getSize();
     	}
-    	int& operator[] (int index) const {
+    	T& operator[] (int index) const {
             if (index < 0 || index >= size) { 
                 cout << "Invalid index" << endl;
                 return arr[0]; 
             }
             return arr[index];
-            
         }
-        
-        friend ostream& operator<<(ostream& out, const DynamicIntegerArray& arr) {
-            
+        friend ostream& operator<<( ostream& out, DynamicArray& arr){
             int size=arr.getSize();
     		for(int i=0;i<size;i++){
     		    cout<<arr[i]<<" ";
     		}
     		return out;
-
     	}
-
 };
 
-int main() {
-    DynamicIntegerArray numbers(3),newNumber(5); 
-    numbers.add(0, 10);
-    numbers.add(1, 20); 
-    numbers.add(2, 30); 
-    numbers.add(3, 40); 
+int main(){
+    DynamicArray<string> numbers(3),newNumber(5);
+    
+    numbers.add(0, "zero");
+    numbers.add(1, "one"); 
+    numbers.add(2, "two"); 
+    numbers.add(3, "three"); 
     if(newNumber<numbers){
         cout<<"true"<<endl;
     }
     cout<<numbers[3]<<endl;
     cout<<numbers<<endl;
     numbers.clear(); 
+    A* a = new A();
+    DynamicArray<A*> aArray(3);
+    aArray.add(0,a);
+    //delete aArray;
     return 0;
 }
