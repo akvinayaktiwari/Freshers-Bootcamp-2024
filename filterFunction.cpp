5d6119c91af42b8bebe7bee5225c42c7a8e8eb79
akvinayaktiwari <64476491+akvinayaktiwari@users.noreply.github.com>
2024-02-01 14:56:12 +0530

    Update filterFunction.cpp

--- filterFunction.cpp
+++ filterFunction.cpp
@@ -1,20 +1,35 @@
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
+##include<bits/stdc++.h>
+using namespace std; 
 
-vector<int> filter(const vector<int>& arr,function<bool<int>> condition){
-    vector<int> results;
-    for(auto element:arr){
-        if(condition(element)){
-            results.push_back(elements);
-        }
-    }
-    return results;
+function<bool(const string&)> isDivisibleBy(int divisor) {
+    return [divisor](const string& str) {
+        return str.length() % divisor == 0;
+    };
 }
 
+vector<string> filter(const vector<string>& strings, const function<bool(const string&)>& criteria) {
+    vector<string> result;
+    for (const string& str : strings) {
+        if (criteria(str)) {
+            result.push_back(str);
+        }
+    }
+    return result;
+}
+
+void printDivisibleStrings(const vector<string>& divisibleStrings) {
+    for (const string& str : divisibleStrings) {
+        cout << str << " ";
+    }
+    cout << endl;
+}
 
 int main() {
+    vector<std::string> strings = {"hello", "india", "wolrd", "any","things"};
+    auto isDivisibleBy3 = isDivisibleBy(3);
     
-
+    vector<string> divisibleStrings = filter(strings, isDivisibleBy3);
+    printDivisibleStrings(divisibleStrings);
+    
     return 0;
 }
